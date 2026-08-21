33a2d97ff525c62aa70bd2e1f9aa965198b36235
sean <snqnn4@gmail.com>
2026-08-20 20:52:44 -0500

    comments

--- services/crypto/cryptoservicer.h
+++ services/crypto/cryptoservicer.h
@@ -10,7 +10,6 @@
 // standard library include: note that expectation results (like encrypt() types) need to be registered as a meta type if queueing signals/slots
 // qRegisterMetaType<expected<types>>(): for queued connections (signal emitted in background)
 
-// derivation uses crypto_pwhash_ALG_ARGON2ID3
 class CryptoServicer : public QObject {
     Q_OBJECT
 public:
